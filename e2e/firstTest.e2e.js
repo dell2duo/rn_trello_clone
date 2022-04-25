da87a8663db8edc860c40d66b1bd8038c58c4c8b
Rafael Rocha <rafael.rocha.ufv@gmail.com>
2022-04-25 15:06:49 -0300

    e2e test for creating a new board

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -3,15 +3,23 @@ describe('Example', () => {
     await device.launchApp()
   })
 
-  beforeEach(async () => {
-    await device.reloadReactNative()
-  })
+  // beforeEach(async () => {
+  //   await device.reloadReactNative()
+  // })
 
   it('should create a new board', async () => {
     await element(by.id('add-board-button')).tap()
     await element(by.label('new-board-input')).typeText('Detox Test Board')
-    await element(by.label('create-board-button')).tap()
+    await element(by.label('create-board-button')).multiTap(2)
     await expect(element(by.text('Detox Test Board'))).toExist()
     await expect(element(by.text('Detox Test Board'))).toBeVisible()
   })
+
+  // it('should check for the board created', async () => {
+  //   await expect(element(by.text('Detox Test Board'))).toExist()
+  //   await expect(element(by.text('Detox Test Board'))).toBeVisible()
+  //   await element(by.id('add-board-button')).tap()
+  //   await element(by.label('new-board-input')).typeText('Detox Test Board 2')
+  //   await element(by.label('create-board-button')).tap()
+  // })
 })
